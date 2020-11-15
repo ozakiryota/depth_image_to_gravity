bd0abf9f8d68489f52498f310e62a1a31e570cc9
ozakiryota <kb22056@gmail.com>
2020-11-16 00:44:01 +0900

    updated

--- pysrc/common/data_transform_mod.py
+++ pysrc/common/data_transform_mod.py
@@ -18,10 +18,11 @@ class DataTransform():
             acc_numpy = self.rotateVector(acc_numpy, -heading_rad)
         ## img: numpy -> tensor
         depth_img_numpy = depth_img_numpy.astype(np.float32)
-        depth_img_numpy = np.where(depth_img_numpy > 0.0, 1.0/depth_img_numpy, depth_img_numpy)
+        # depth_img_numpy = np.where(depth_img_numpy > 0.0, 1.0/depth_img_numpy, depth_img_numpy)
         # depth_img_numpy = np.clip(depth_img_numpy, 0, 1)
         depth_img_tensor = torch.from_numpy(depth_img_numpy)
         depth_img_tensor = depth_img_tensor.unsqueeze_(0)
+        # depth_img_tensor = transforms.functional.normalize(depth_img_tensor, ([0.5]), ([0.5]))
         ## acc: numpy -> tensor
         acc_numpy = acc_numpy.astype(np.float32)
         acc_numpy = acc_numpy / np.linalg.norm(acc_numpy)
@@ -33,8 +34,8 @@ class DataTransform():
         slide_pixel = random.randint(0, depth_img_numpy.shape[1] - 1)
         heading_rad = self.anglePiToPi(2 * math.pi / depth_img_numpy.shape[1] * slide_pixel)
         slid_depth_img_numpy = np.roll(depth_img_numpy, slide_pixel, axis=1)
-        # print("slide_pixel = ", slide_pixel)
-        # print("heading_rad/math.pi*180.0 = ", heading_rad/math.pi*180.0)
+        print("slide_pixel = ", slide_pixel)
+        print("heading_rad/math.pi*180.0 = ", heading_rad/math.pi*180.0)
         return slid_depth_img_numpy, heading_rad
 
     def rotateVector(self, acc_numpy, angle):
@@ -50,32 +51,34 @@ class DataTransform():
         return math.atan2(math.sin(angle), math.cos(angle))
 
 ##### test #####
-## depth image
-depth_img_path = "../../../dataset_image_to_gravity/AirSim/lidar/example.npy"
-depth_img_numpy = np.load(depth_img_path)
-print("depth_img_numpy = ", depth_img_numpy)
-print("depth_img_numpy.shape = ", depth_img_numpy.shape)
-## label
-acc_list = [1, 0, 0]
-acc_numpy = np.array(acc_list)
-print("acc_numpy = ", acc_numpy)
-## transform
-transform = DataTransform()
-depth_img_trans, acc_trans = transform(depth_img_numpy, acc_numpy, phase="train")
-print("acc_trans = ", acc_trans)
-## tensor -> numpy
-depth_img_trans_numpy = depth_img_trans.numpy().transpose((1, 2, 0))  #(ch, h, w) -> (h, w, ch)
-print("depth_img_trans_numpy.shape = ", depth_img_trans_numpy.shape)
-## save
-depth_img_trans_numpy = depth_img_trans_numpy.squeeze(2)
-img_pil = Image.fromarray(np.uint8(255*depth_img_trans_numpy/np.max(depth_img_trans_numpy)))
-save_path = "../../save/transform.jpg"
-img_pil.save(save_path)
-print("saved: ", save_path)
-## imshow
-plt.figure()
-plt.subplot(2, 1, 1)
-plt.imshow(depth_img_numpy)
-plt.subplot(2, 1, 2)
-plt.imshow(depth_img_trans_numpy)
-plt.show()
+# ## depth image
+# depth_img_path = "../../../dataset_image_to_gravity/AirSim/lidar/example.npy"
+# depth_img_numpy = np.load(depth_img_path)
+# print("depth_img_numpy = ", depth_img_numpy)
+# print("depth_img_numpy.shape = ", depth_img_numpy.shape)
+# ## label
+# acc_list = [1, 0, 0]
+# acc_numpy = np.array(acc_list)
+# print("acc_numpy = ", acc_numpy)
+# ## transform
+# transform = DataTransform()
+# depth_img_trans, acc_trans = transform(depth_img_numpy, acc_numpy, phase="train")
+# print("depth_img_trans = ", depth_img_trans)
+# print("depth_img_trans.size() = ", depth_img_trans.size())
+# print("acc_trans = ", acc_trans)
+# ## tensor -> numpy
+# depth_img_trans_numpy = depth_img_trans.numpy().transpose((1, 2, 0))  #(ch, h, w) -> (h, w, ch)
+# print("depth_img_trans_numpy.shape = ", depth_img_trans_numpy.shape)
+# ## save
+# depth_img_trans_numpy = depth_img_trans_numpy.squeeze(2)
+# img_pil = Image.fromarray(np.uint8(255*depth_img_trans_numpy/np.max(depth_img_trans_numpy)))
+# save_path = "../../save/transform.jpg"
+# img_pil.save(save_path)
+# print("saved: ", save_path)
+# ## imshow
+# plt.figure()
+# plt.subplot(2, 1, 1)
+# plt.imshow(depth_img_numpy)
+# plt.subplot(2, 1, 2)
+# plt.imshow(depth_img_trans_numpy)
+# plt.show()
